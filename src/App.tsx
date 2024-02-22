6a49bfee8a436d95cfb52277ed5cf843ddedad03
Nishanth Jayram <nishanthjayram@gmail.com>
2024-02-21 17:49:39 -0800

    Set up app component with pixmap canvas.
    We now have our state and dispatch function, as defined by our reducer.
    We pass the state and appropriate dispatch calls down to the
    PixmapCanvas component, which will handle rendering and making updates
    to the state based on user interaction.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import styles from "./App.module.css";
+import { Pixmap } from "./Pixmap";
+import PixmapCanvas from "./components/PixmapCanvas/PixmapCanvas";
+import { TState } from "./types";
+import { stateReducer } from "./utils/stateReducer";
+
+const initializeState = (): TState => ({
+  pixmap: new Pixmap(640, 442),
+  isDrawing: false,
+  lastPos: null,
+  tool: "pen",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [state, dispatch] = useReducer(stateReducer, initializeState());
   return (
-    <>
+    <div className={styles.app}>
+      <div className={styles.canvasWrapper}>
+        <PixmapCanvas
+          pixmap={state.pixmap}
+          onMouseDown={(e) => {
+            dispatch({
+              type: "mousedown",
+              payload: [e.nativeEvent.offsetX, e.nativeEvent.offsetY],
+            });
+          }}
+          onMouseUp={() => {
+            dispatch({ type: "mouseup" });
+          }}
+          onMouseMove={(e) => {
+            dispatch({
+              type: "mousemove",
+              payload: [e.nativeEvent.offsetX, e.nativeEvent.offsetY],
+            });
+          }}
+        />
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            dispatch({ type: "setTool", payload: "pen" });
+          }}
+        >
+          Pen
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
