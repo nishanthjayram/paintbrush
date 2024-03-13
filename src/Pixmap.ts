ae54642ad456ffde5ea92c0e78d33deffe686cae
Nishanth Jayram <nishanthjayram@gmail.com>
2024-03-13 15:50:44 -0700

    Added draw methods for [filled] rectangle and ellipse.

--- src/Pixmap.ts
+++ src/Pixmap.ts
@@ -50,6 +50,100 @@ export class Pixmap {
     return this;
   }
 
+  drawRectangle(
+    x0: number,
+    y0: number,
+    x1: number,
+    y1: number,
+    borderColor: number
+  ) {
+    this.drawLine(x0, y0, x1, y0, borderColor);
+    this.drawLine(x1, y0, x1, y1, borderColor);
+    this.drawLine(x1, y1, x0, y1, borderColor);
+    this.drawLine(x0, y1, x0, y0, borderColor);
+
+    return this;
+  }
+
+  drawFilledRectangle(
+    x0: number,
+    y0: number,
+    x1: number,
+    y1: number,
+    fillColor: number,
+    borderColor: number
+  ) {
+    this.drawRectangle(x0, y0, x1, y1, borderColor);
+
+    const [xMin, xMax] = x0 < x1 ? [x0, x1] : [x1, x0];
+    const [yMin, yMax] = y0 < y1 ? [y0, y1] : [y1, y0];
+
+    for (let y = yMin + 1; y < yMax; y++) {
+      this.drawLine(xMin + 1, y, xMax - 1, y, fillColor);
+    }
+
+    return this;
+  }
+
+  drawEllipse(
+    x0: number,
+    y0: number,
+    rx: number,
+    ry: number,
+    borderColor: number
+  ) {
+    let [x, y] = [0, ry];
+
+    let d1 = ry * ry - rx * rx * ry + 0.25 * rx * rx;
+    let dx = 2 * ry * ry * x;
+    let dy = 2 * rx * rx * y;
+
+    while (dx < dy) {
+      this.setPixel(x + x0, y + y0, borderColor);
+      this.setPixel(-x + x0, y + y0, borderColor);
+      this.setPixel(x + x0, -y + y0, borderColor);
+      this.setPixel(-x + x0, -y + y0, borderColor);
+
+      if (d1 < 0) {
+        x++;
+        dx = dx + 2 * ry * ry;
+        d1 = d1 + dx + ry * ry;
+      } else {
+        x++;
+        y--;
+        dx = dx + 2 * ry * ry;
+        dy = dy - 2 * rx * rx;
+        d1 = d1 + dx - dy + ry * ry;
+      }
+    }
+
+    let d2 =
+      ry * ry * ((x + 0.5) * (x + 0.5)) +
+      rx * rx * ((y - 1) * (y - 1)) -
+      rx * rx * ry * ry;
+
+    while (y >= 0) {
+      this.setPixel(x + x0, y + y0, borderColor);
+      this.setPixel(-x + x0, y + y0, borderColor);
+      this.setPixel(x + x0, -y + y0, borderColor);
+      this.setPixel(-x + x0, -y + y0, borderColor);
+
+      if (d2 > 0) {
+        y--;
+        dy = dy - 2 * rx * rx;
+        d2 = d2 + rx * rx - dy;
+      } else {
+        y--;
+        x++;
+        dx = dx + 2 * ry * ry;
+        dy = dy - 2 * rx * rx;
+        d2 = d2 + dx - dy + rx * rx;
+      }
+    }
+
+    return this;
+  }
+
   copy() {
     const copy = new Pixmap(this.width, this.height);
     copy.pixels.set(this.pixels);
