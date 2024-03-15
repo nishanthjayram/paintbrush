7efae468e8f3d4bcca354b78b8fa340c6d005d05
Nishanth Jayram <nishanthjayram@gmail.com>
2024-03-14 20:07:13 -0700

    Defined a separate util for "getting" lines.
    Since erasing will need to mirror the behavior of free drawing, they
    both require Bresenham's line-drawing algorithm. Thus, to modularize
    things, I've defined a separate utility function that will "get" the
    line, i.e. return the set of points, without actually drawing it.

---
+++ src/utils/getLine.ts
@@ -0,0 +1,29 @@
+import { TPos } from "../types";
+
+export const getLine = ([x0, y0]: TPos, [x1, y1]: TPos) => {
+  const points: TPos[] = [];
+  let x = x0;
+  let y = y0;
+  const dx = Math.abs(x1 - x0);
+  const sx = x0 < x1 ? 1 : -1;
+  const dy = -Math.abs(y1 - y0);
+  const sy = y0 < y1 ? 1 : -1;
+  let err = dx + dy;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    points.push([x, y]);
+    if (x === x1 && y === y1) break;
+    const e2 = 2 * err;
+    if (e2 >= dy) {
+      err += dy;
+      x += sx;
+    }
+    if (e2 <= dx) {
+      err += dx;
+      y += sy;
+    }
+  }
+
+  return points;
+};
